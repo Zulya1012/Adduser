f8eadecfbc25d3c6d834f7ca3bc05afd455da3fa
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-31 19:01:19 +0400

    Update adduser.java

--- adduser.java
+++ adduser.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
-public class adduser{
-	public static void main(String[] arg){
-		String [] username={"Sun", "Moon", "Star", "Moon"};
-		int count=0;
-		Scanner s=new Scanner(System.in);
-		String a=s.nextLine();
-		for (int i=0; i<username.length; i++){
-			if(username[i].equals(a))
-				count=count+1;
-			
-		}
-		if(count==0){
-			System.out.println("This username isn't in the array");
-		}
-		else{
-			System.out.println(count);
-		}
-	}	
+public class adduser {
+  public static void main(String[] arg) {
+    String[] username = {
+      "Sun",
+      "Moon",
+      "Star",
+      "Moon"
+    };
+    int count = 0;
+    Scanner s = new Scanner(System.in);
+    String a = s.nextLine();
+    for (int i = 0; i < username.length; i++) {
+      if (username[i].equals(a))
+        count = count + 1;
+
+    }
+    if (count == 0) {
+      System.out.println("This username isn't in the array");
+    } else {
+      System.out.println(count);
+    }
+  }
 }
